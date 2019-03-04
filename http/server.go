466f6f009dc027969ded17749210abade8235dc2
Minhaz Syrus <msyrus@gmail.com>
2019-03-04 22:50:30 +0600

    Refactor ManageServer

--- http/server.go
+++ http/server.go
@@ -15,44 +15,37 @@ import (
 // has expired it stops the server immedietly. It returns any error that is returned
 // by the srvr
 func ManageServer(srvr *http.Server, gracePeriod time.Duration) error {
-	sigCh := make(chan os.Signal, 0)
 	errCh := make(chan error, 0)
+
+	sigs := []os.Signal{syscall.SIGINT, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGTERM, os.Interrupt}
+
+	graceful := func() error {
+		log.Println("Suttingdown server gracefully with in", gracePeriod)
+		log.Println("To shutdown immedietly press again")
+
+		ctx, cancel := context.WithTimeout(context.Background(), gracePeriod)
+		defer cancel()
+
+		return srvr.Shutdown(ctx)
+	}
+
+	forced := func() error {
+		log.Println("Suttingdown server forcefully")
+		return srvr.Close()
+	}
+
 	go func() {
-		log.Println("Starting web server on", srvr.Addr)
-		errCh <- srvr.ListenAndServe()
+		log.Println("Starting server on", srvr.Addr)
+		if err := srvr.ListenAndServe(); err != http.ErrServerClosed {
+			errCh <- err
+		}
 	}()
 
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGTERM, os.Interrupt)
+	go func() {
+		errCh <- HandleSignals(sigs, graceful, forced)
+	}()
 
-	for i := 0; i < 2; i++ {
-		select {
-		case err := <-errCh:
-			return err
-		case <-sigCh:
-			if i == 0 {
-				d := gracePeriod
-				log.Println("Suttingdown server gracefully with in", d)
-				log.Println("To shutdown immedietly press again")
-				go func() {
-					ctx, cancel := context.WithTimeout(context.Background(), d)
-					defer cancel()
-
-					if err := srvr.Shutdown(ctx); err != nil {
-						errCh <- err
-						return
-					}
-					errCh <- nil
-				}()
-				continue
-			}
-
-			log.Println("Suttingdown web server forcefully")
-			if err := srvr.Close(); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
+	return <-errCh
 }
 
 // HandleSignals listen on the registered signals and fires the gracefulHandler for the
